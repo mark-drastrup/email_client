53b453666a1d07889bc77b7f695a929e4669a200
mark-drastrup <mark.drastrup@gmail.com>
2019-11-10 22:40:51 +0900

    Importing api key and client id from .env file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
-    <h1>Email Client</h1>
+    <h1>Gmail API demo</h1>
+
+      <button id="authorize-button" class="btn btn-primary hidden">Authorize</button>
+
+      <table class="table table-striped table-inbox hidden">
+        <thead>
+          <tr>
+            <th>From</th>
+            <th>Subject</th>
+            <th>Date/Time</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    console.log(process.env.VUE_APP_CLIENT_ID)
+    console.log(process.env.VUE_APP_API_KEY)
+  }
 }
 </script>
 
